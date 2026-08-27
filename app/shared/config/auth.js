f6b339e79f18f86f5e829c3d8e85e5e2729cc67c
Daniel <da.aldanag@gmail.com>
2026-08-27 15:18:10 -0600

    Daniel: aclaracion de cookieMaxAge

--- app/shared/config/auth.js
+++ app/shared/config/auth.js
@@ -3,5 +3,5 @@ module.exports = {
     refreshSecret: process.env.JWT_REFRESH_SECRET,
     accessExpiresIn: process.env.JWT_ACCESS_EXPIRES_IN || '15m',
     refreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN || '7d',
-    cookieMaxAge: 7 * 24 * 60 * 60 * 1000
+    cookieMaxAge: 7 * 24 * 60 * 60 * 1000 // Tiempo maximo de token refresh para cookie
 };
